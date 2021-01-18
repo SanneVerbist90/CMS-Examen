b9e7fb9500b4bc5ed222da44bc1e81d404eec41b
SanneVerbist90 <60688549+SanneVerbist90@users.noreply.github.com>
2021-01-18 14:42:09 +0100

    header werkt

---
+++ sannes-wolwinkel/src/components/header/header.js
@@ -0,0 +1,52 @@
+import React from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import PropTypes from "prop-types"
+import {HeaderWrapper, Image} from './headerStyles/headerStyles'
+import Menu from './Menu' 
+
+const Header = ({ siteTitle }) => {
+  const {
+    logo,
+    wpcontent: {menuItems},
+     } = useStaticQuery(graphql`
+  query {
+    logo: file(relativePath: {eq: "logo2.png"}) {
+      childImageSharp {
+      fixed(quality: 100, height: 80){
+        ...GatsbyImageSharpFixed_withWebp
+      }
+    }
+  }
+      
+    wpcontent {
+      menuItems {
+        edges {
+          node {
+            label
+            path
+          }
+        }
+      }
+    }
+  }
+  `)
+
+   return (
+   <HeaderWrapper>
+    <Link to="/">
+    <Image alt="logo artist agency" fixed={logo.childImageSharp.fixed} /> 
+    </Link>
+       <Menu menuItems={menuItems.edges} /> 
+    </HeaderWrapper>
+   )
+}
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
